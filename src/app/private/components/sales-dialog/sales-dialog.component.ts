be63448f0e4ce88ab0e6fb2a81e9d22a18aa5586
andreacebrianbabel <andrea.cebrian@babelgroup.com>
2023-04-13 06:39:29 +0200

    venta hecha

--- src/app/private/components/sales-dialog/sales-dialog.component.ts
+++ src/app/private/components/sales-dialog/sales-dialog.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ResultsService } from '../../services/results.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CryptoData } from '../../models/crypto-model';
+import { Relation } from '../../models/relation-model';
 
 @Component({
   selector: 'app-sales-dialog',
@@ -7,9 +12,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalesDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(public fb: FormBuilder, private resultsService: ResultsService, private snackbar: MatSnackBar) { }
 
-  ngOnInit(): void {
+  public cryptoSale = this.fb.group({
+    crypto: ['', Validators.required],
+    crypto_quantity: ['', Validators.required]
+  })
+
+  cryptos: CryptoData[]
+  totalSalePrice: number
+  userId = '0'
+  userIdSS = sessionStorage.getItem('user_id')
+  newRelation: Relation
+
+  getAllCryptos() {
+    this.resultsService.getAllCryptos().subscribe(
+      (crypto) => {
+        this.cryptos = crypto
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
   }
 
+  ngOnInit(): void {
+    if (!!this.userIdSS)
+      this.userId = this.userIdSS
+    this.getAllCryptos()
+
+    this.cryptoSale.get('crypto_quantity')?.valueChanges.subscribe(
+      (value: number) => {
+        const cryptoType = this.cryptoSale.get('crypto')?.value.crypto_id
+        const price = this.cryptos.find(element => element.crypto_id === cryptoType)?.value
+
+        this.totalSalePrice = Number((price! * value).toFixed(3))
+      }
+    )
+  }
+
+  openSaleBar() {
+    this.snackbar.open('¡La venta se ha realizado exitosamente!', '', {
+      duration: 2500
+    })
+  }
 }
